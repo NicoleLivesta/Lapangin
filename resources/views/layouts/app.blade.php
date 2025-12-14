3d5d9f95dbf4c1f949d8aa73235924dcbec9b4e9
Nicole Livesta <nicole24ti@mahasiswa.pcr.ac.id>
2025-12-14 23:24:53 +0700

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Booking Lapangan</title>
+
+    <!-- Google Material Symbols (WAJIB untuk icon) -->
+    <link rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+    <!-- CSS dari Material Kit -->
+    <link href="{{ asset('assets/css/material-kit.css') }}" rel="stylesheet">
+</head>
+<body>
+
+    @yield('content')
+
+    <!-- JS dari Material Kit -->
+    <script src="{{ asset('assets/js/material-kit.min.js') }}"></script>
+</body>
+</html>
