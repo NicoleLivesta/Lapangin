66832768d140474d0397aad25a76a2094a0629a7
Nicole Livesta <nicole24ti@mahasiswa.pcr.ac.id>
2025-12-22 23:51:52 +0700

    Progress 2

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,36 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/lapangan', function () {
+    return view('lapangan');
+});
+
+Route::get('/jadwal', function () {
+    return view('jadwal');
+});
+
+Route::get('/cara-booking', function () {
+    return view('cara-booking');
+});
+
+require __DIR__.'/auth.php';
