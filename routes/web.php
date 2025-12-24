b69959d22b32a85fe7d0d47d182617b9e882a678
Nicole Livesta <nicole24ti@mahasiswa.pcr.ac.id>
2025-12-25 01:08:02 +0700

    Progress2

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/', function () {
-    return view('home');
-});
-
 Route::get('/lapangan', function () {
     return view('lapangan');
 });
